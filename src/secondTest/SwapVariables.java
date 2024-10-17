6e852867eeb7f1050da9510b7c5a2c3270f3a060
BAKHTOVAR <113468503+Abdulloev-Bakhtovar@users.noreply.github.com>
2024-10-17 15:40:10 +0500

    Create SwapVariables.java

---
+++ src/secondTest/SwapVariables.java
@@ -0,0 +1,13 @@
+public class SwapVariables {
+    public static void main(String[] args) {
+        int a = 8;
+        int b = 10;
+
+        a = a + b; 
+        b = a - b; 
+        a = a - b; 
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b); 
+    }
+}
